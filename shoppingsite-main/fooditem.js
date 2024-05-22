d21e44af9b18ce3a331e66ba4b1530b8f3b3759c
Demi99F <101418339+Demi99F@users.noreply.github.com>
2024-05-22 21:01:17 +0200

    Add files via upload

---
+++ shoppingsite-main/fooditem.js
@@ -0,0 +1,198 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'MUSHROOM SWISS',
+    category : 'burgers',
+    rating : 4.3,
+    price: 13,
+    img: 'images/burgers/mushroom.avif',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'TRIPLE BACON',
+    category : 'burgers',
+    rating : 4.3,
+    price: 15,
+    img: 'images/burgers/triplebacon.avif',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'ALL-AMERICAN',
+    category : 'burgers',
+    rating : 4.3,
+    price: 18,
+    img: 'images/burgers/cheese.avif',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'CURD BACON',
+    category : 'burgers',
+    rating : 4.3,
+    price: 15,
+    img: 'images/burgers/curd.avif',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'SMOKED BRISKET',
+    category : 'burgers',
+    rating : 4.3,
+    price: 10,
+    img: 'images/burgers/smoked.avif',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'BACON SMASHED',
+    category : 'burgers',
+    rating : 4.3,
+    price: 12,
+    img: 'images/burgers/smashed.avif',
+    quantity: 1
+},
+{
+    id: 7,
+    name: 'SOUTHWESTERN',
+    category : 'burgers',
+    rating : 4.3,
+    price: 13,
+    img: 'images/burgers/bean.avif',
+    quantity: 1
+},
+
+{
+    id: 8,
+    name: 'FRENCH FRIES',
+    category : 'sides',
+    rating : 4.3,
+    price: 11,
+    img: 'images/sides/fries.avif',
+    quantity: 1
+},
+{
+    id: 9,
+    name: 'POTATO WEDGES',
+    category : 'sides',
+    rating : 4.3,
+    price: 10,
+    img: 'images/sides/potato.avif',
+    quantity: 1
+},
+{
+    id: 10,
+    name: 'MAC & CHEESE',
+    category : 'sides',
+    rating : 4.3,
+    price: 14,
+    img: 'images/sides/mac.avif',
+    quantity: 1
+},
+{
+    id: 11,
+    name: 'ONION RINGS',
+    category : 'sides',
+    rating : 4.3,
+    price: 12,
+    img: 'images/sides/onion.avif',
+    quantity: 1
+},
+{
+    id: 12,
+    name: 'CHEESE CURDS',
+    category : 'sides',
+    rating : 4.3,
+    price: 17,
+    img: 'images/sides/curds.avif',
+    quantity: 1
+},
+{
+    id: 13,
+    name: 'TOTS',
+    category : 'sides',
+    rating : 4.3,
+    price: 20,
+    img: 'images/sides/tots.avif',
+    quantity: 1
+},
+{
+    id: 14,
+    name: 'PEPSI',
+    category : 'drinks',
+    rating : 4.3,
+    price: 15,
+    img: 'images/drinks/pepsi.avif',
+    quantity: 1
+
+},
+{
+    id: 15,
+    name: 'MOUNTAIN DEW',
+    category : 'drinks',
+    rating : 4.3,
+    price: 10,
+    img: 'images/drinks/mountian.avif',
+    quantity: 1
+},
+{
+    id: 16,
+    name: 'DR PEPPER',
+    category : 'drinks',
+    rating : 4.3,
+    price: 15,
+    img: 'images/drinks/pepper.avif',
+    quantity: 1
+
+},
+{
+    id: 17,
+    name: 'STARRY',
+    category : 'drinks',
+    rating : 4.3,
+    price: 12,
+    img: 'images/drinks/starry.avif',
+    quantity: 1
+
+},
+{
+    id: 18,
+    name: 'LEMONADE',
+    category : 'drinks',
+    rating : 4.3,
+    price: 8,
+    img: 'images/drinks/lemonade.avif',
+    quantity: 1
+},
+{
+    id: 19,
+    name: 'ICED TEA',
+    category : 'drinks',
+    rating : 4.3,
+    price: 7,
+    img: 'images/drinks/icedtea.avif',
+    quantity: 1
+},
+{
+    id: 20,
+    name: 'RED BULL',
+    age: '32',
+    category : 'drinks',
+    rating : 4.3,
+    price: 5,
+    img: 'images/drinks/redbull.avif',
+    quantity: 1
+},
+{
+    id: 21,
+    name: 'SPARKLING WATER',
+    category : 'drinks',
+    rating : 4.3,
+    price: 8,
+    img: 'images/drinks/water.avif',
+    quantity: 1
+}
+]
+
+export {foodItem};
